dc2c75aec8a2e31e848dd6ede868db219ab9bbb7
piyushnh <piyush.n.h@gmail.com>
2021-02-27 12:34:43 +0530

    Add: React-swipeable-views library
    Signed-off-by: piyushnh <piyush.n.h@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState} from 'react';
+import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
+import axios from 'axios';
+import SwipeableViews from 'react-swipeable-views';
+
+const styles = {
+  slideContainer: {
+    height: '100vh',
+    // marginTop: '25%'
+  },
+  slide: {
+    // padding: 15,
+    marginTop:"10vh",
+    marginBottom:"10vh",
+    minHeight: '80vh',
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#E3DC4B',
+  },
+  scroll: {
+    height: 100,
+    overflow: 'scroll',
+  }
+};
+
 
 function App() {
+
+  const [images, setImage] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios.get(`http://res.cloudinary.com/keepup/image/list/test.json`);
+      console.log(response)
+      let imageList = response.data.resources
+      setImage([...imageList])
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SwipeableViews containerStyle={styles.slideContainer} axis="y" enableMouseEvents resistance>
+ 
+     <div style={Object.assign({}, styles.slide, styles.slide1)}>slide n°1</div>
+    <div style={Object.assign({}, styles.slide, styles.slide2)}>slide n°2</div>
+    <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n°3</div>
+  {/* <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n°3</div> */}
+  </SwipeableViews>
   );
 }
-
+{/* <div>
+<CloudinaryContext cloudName="keepup">
+<SwipeableViews containerStyle={styles.slideContainer} axis="y" enableMouseEvents>
+    {images.map((item) => 
+         <Image publicId={item.public_id} style={Object.assign({}, styles.slide, styles.slide1)} />
+         )
+       } 
+    <div style={Object.assign({}, styles.slide, styles.slide1)}>slide n°1</div>
+<div style={Object.assign({}, styles.slide, styles.slide2)}>slide n°2</div>
+ <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n°3</div>
+ </SwipeableViews>
+ </CloudinaryContext>
+</div> */}
 export default App;
